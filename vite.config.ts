b3080a69940cdabdc08523b4936aec6677ec8de9
Cory Fail <me@coryfail.com>
2025-06-10 21:54:55 -0400

    fix package

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
-import routify from "@roxi/routify/vite-plugin"; // Optional, remove if unused
+import routify from "@roxi/routify/vite-plugin";
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
   plugins: [
     tailwindcss(),
-    routify({}), // Optional
+    routify({
+      /* config */
+    }),
     svelte(),
   ],
 });
