415d8e789b5c43b37bd82cfd765c78b2540b6da3
Cory Fail <me@coryfail.com>
2025-06-10 22:20:49 -0400

    Fix issue with redirects

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
 
+if (sessionStorage.redirect) {
+  const redirectUrl = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  history.replaceState(null, "", redirectUrl);
+}
+
 const app = mount(App, {
   target: document.getElementById("app")!,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     routify({
-      /* config */
+      restoreScrollState: true,
     }),
     svelte(),
   ],
