9a30502a71397abb010908927fc56b0a87f51a76
Cory Fail <me@coryfail.com>
2025-06-10 21:19:19 -0400

    Updates

---
+++ assets/index-CPmBjsmS-DFc0aJ24.js
@@ -0,0 +1,12 @@
+import{R as g,M as $,b as B,I as O,D as f,U as h,a as R,u as o,B as d,v as A,_ as t,c as D,F as x,x as w,p as Y}from"./index-DjsnoBaY.js";import{C as j}from"./BlogPost-CkPxSWNx-B79dct-L.js";import{Y as F}from"./MetaTags-DiGhpb5G-DNs7e65x.js";import{O as z,R as G}from"./Placeholder-aSgtxXEm-BwbuTU5m.js";import"./index-HNmk3Fiy-Cjg0-jCP.js";var L=h('<section class="flex items-center justify-center"><div class="max-w-2xl px-6"><h1 class="text-4xl">Building Software for Social Good</h1> <p class="py-6 text-lg p-4 block"></p></div></section>');function M(i,l){g(l,!1);const r=new Date().getFullYear()-2010;f();var e=L(),c=d(e),s=o(d(c),2);s.textContent=`I'm Cory Chris, a technology leader and founder with ${r??""} years
+      of experience creating real-world impact through software. I lead teams, scale
+      platforms, and spearhead projects that make life better, from modernizing government
+      systems to building Open Source tools and developing ethical AI for social
+      good. My work focuses on using AI to fight misinformation, expand access to
+      education, and strengthen communities. I believe great software, especially
+      when powered by thoughtful, human-centered AI, can make the world more accessible,
+      equitable, and truly human.`,x(i,e),w()}var P=h('<!> <!> <section class="max-w-2xl mx-auto px-4 py-10"><h1 class="text-4xl mb-6">Latest Posts</h1> <!> <!></section>',1);function E(i,l){g(l,!1);let r={items:[]},e=$([]);B(async()=>{r=await z.collection("posts").getList(1,3,{sort:"-created"}),O(e,r.items)});const c=new Date().getFullYear()-2010;f();var s=P(),m=R(s);F(m,{title:"Building Software for Social Good - Cory Chris",description:`I'm Cory Chris, a technology leader and founder with ${c??""} years
+      of experience creating real-world impact through software. I lead teams, scale
+      platforms, and spearhead projects that make life better, from modernizing government
+      systems to building Open Source tools and developing ethical AI for social
+      good.`});var p=o(m,2);M(p,{});var y=o(p,2),u=o(d(y),2);{var v=a=>{G(a,{type:"blogPost",amount:3})};A(u,a=>{t(e).length===0&&a(v)})}var b=o(u,2);D(b,1,()=>t(e),Y,(a,n)=>{let I=()=>t(n).slug,C=()=>t(n).title,S=()=>t(n).created,k=()=>t(n).excerpt;j(a,{get slug(){return I()},get title(){return C()},get date(){return S()},get excerpt(){return k()}})}),x(i,s),w()}export{E as default};
